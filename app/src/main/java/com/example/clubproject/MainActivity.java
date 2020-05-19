6e6fa9c374a6ef547caa92cb0beb72eb7041383d
Nathan Dilla <natedillamc@gmail.com>
2020-05-19 12:43:24 -0700

    intial commit implemented jsoup

---
+++ app/src/main/java/com/example/clubproject/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.clubproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new doIt().execute();
+            }
+        });
+    }
+
+    public class doIt extends AsyncTask<Void, Void, Void>{
+
+        String words;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+
+                Document doc = Jsoup.connect("https://www.imdb.com/name/nm0447695/").get();
+
+                words = doc.text();
+
+            }
+            catch(Exception e){e.printStackTrace();}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            text.setText(words);
+        }
+    }
+}
