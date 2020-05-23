8d470b975934e05df1409c945ec6bdd7b9dabac0
Nathan Dilla <natedillamc@gmail.com>
2020-05-23 15:40:33 -0700

    scanned for elements and parses text. now shows clubs list on button press.

--- app/src/main/java/com/example/clubproject/MainActivity.java
+++ app/src/main/java/com/example/clubproject/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
     Button button;
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.textView);
         button = findViewById(R.id.button);
+        layout = findViewById(R.id.mainLayout);
+
+        //Button testButton = new Button(this);
+        //RelativeLayout myLayout = new RelativeLayout(this);
+        //layout.addView(testButton);
+        //setContentView(layout);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,23 +44,41 @@ public class MainActivity extends AppCompatActivity {
                 new doIt().execute();
             }
         });
+
+
     }
 //change
     public class doIt extends AsyncTask<Void, Void, Void>{
 
-        String words;
+        String words = "";
+        Document doc;
 
         @Override
         protected Void doInBackground(Void... voids) {
 
             try {
 
-                Document doc = Jsoup.connect("https://www.imdb.com/name/nm0447695/").get();
+                doc = Jsoup.connect("https://searchusers.com/search/valley+view+high+school").get();
 
-                words = doc.text();
+                //words = doc.text();
 
             }
-            catch(Exception e){e.printStackTrace();}
+            catch(Exception e){e.printStackTrace();
+            }
+
+            Elements elements = doc.getElementsByClass("timg");
+
+            for (Element element : elements)
+            {
+                String text = element.text();
+
+
+
+                words += text;
+                words += "\n";
+               // createButton(text);
+            }
+
             return null;
         }
 
@@ -56,5 +88,21 @@ public class MainActivity extends AppCompatActivity {
 
             text.setText(words);
         }
+        /*
+    private Button createButton(String text)
+    {
+        Button clubButton = new Button(getApplicationContext());
+        //button.setLayoutParams(new RelativeLayout.LayoutParams(150,150));
+        clubButton.setText(text);
+        //clubButton.setLayoutParams(new;
+
+        layout.addView(clubButton);
+
+        return clubButton;
     }
+
+         */
+    }
+
+
 }
